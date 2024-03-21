1487ff7261b1f69e1c6806c9912480bc62fcee09
ddeng <ddeng36@gatech.edu>
2024-03-21 13:35:57 +0800

    add view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,13 +5,14 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
-
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
